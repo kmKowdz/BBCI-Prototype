2e94d9d8ca7471d2ee79bd5d70960cec9d9a36d2
kmKowdz <kmalarcon@live.com>
2021-05-22 15:03:12 +0800

    Add sawtooth python sdk
    added sawtooth python sdk as base build for building my own prototype

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# BBCI Prototype
- A prototype for Blockchain-based Continuous Integration system.
+![Hyperledger Sawtooth](https://raw.githubusercontent.com/hyperledger/sawtooth-core/master/images/sawtooth_logo_light_blue-small.png)
+
+# Hyperledger Sawtooth SDK
+
+*Hyperledger Sawtooth* is an enterprise solution for building, deploying, and
+running distributed ledgers (also called blockchains). It provides an
+extremely modular and flexible platform for implementing transaction-based
+updates to shared state between untrusted parties coordinated by consensus
+algorithms.
+
+The *Sawtooth SDK* provides a number of useful components that simplify
+developing Python applications which interface with the Sawtooth platform.
+For full usage and installation instructions please reference the
+official Sawtooth documentation below:
+
+  * [Hyperledger Sawtooth Official Documentation](https://sawtooth.hyperledger.org/docs/)
+  * [Sawtooth Application Developer's Guide](https://sawtooth.hyperledger.org/docs/core/releases/latest/app_developers_guide.html)
+  * [Sawtooth Python SDK Guide](https://sawtooth.hyperledger.org/docs/core/releases/latest/app_developers_guide/python_sdk.html)
---
+++ examples/intkey_python/setup_tests.py
@@ -0,0 +1,58 @@
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+from __future__ import print_function
+
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+
+
+data_files = []
+
+if os.path.exists("tests"):
+    data_files.append(('/data/tests/intkey', ['tests/test_tp_intkey.py']))
+    data_files.append(('/data/tests/intkey', [
+        '../../tests/sawtooth_integration/tests/test_intkey_smoke.py']))
+
+try:
+    os.environ["ST_VERSION"]
+    print('Using ST_VERSION')
+    VERSION = os.environ["ST_VERSION"]
+except KeyError:
+    print('ST_VERSION not set. Using get_version')
+    VERSION = subprocess.check_output(
+        ['../../bin/get_version']).decode('utf-8').strip()
+
+setup(
+    name='sawtooth-intkey-tests',
+    version=VERSION,
+    description='Sawtooth Intkey Python Test',
+    author='Hyperledger Sawtooth',
+    url='https://github.com/hyperledger/sawtooth-sdk-python',
+    packages=find_packages(),
+    install_requires=[
+        "cbor",
+        "colorlog",
+        "sawtooth-sdk",
+    ],
+    data_files=data_files,
+    entry_points={
+        'console_scripts': [
+            'intkey = sawtooth_intkey.client_cli.intkey_cli:main_wrapper',
+            'intkey-tp-python = sawtooth_intkey.processor.main:main'
+        ]
+    })
